c7dad1ca93c80cf9e5a8f1f22d7d6793a1102eb9
Volokhh <karinochkavolokh@gmail.com>
2026-08-20 19:09:01 +0300

    HW-24_todo_step-2_Events

--- todo-list/main.js
+++ todo-list/main.js
@@ -82,9 +82,10 @@ const taskWrapper = createDomElement('div', {
    className: 'todo__task-wrapper'
 });
 
-const btnDone = createDomElement('button', {
+const btnDone = createDomElement('input', {
    className: 'todo__item-done',
-   textContent: '\u2713'
+   //value: '\u2713',
+   type: 'checkbox',
 });
 
 const itemText = createDomElement('p', {
@@ -94,7 +95,7 @@ const itemText = createDomElement('p', {
 
 const btnItemDelete = createDomElement('button', {
    className: 'todo__item-delete',
-   textContent: '\u0078'
+   textContent: '\u0078',
 });
 
 const date = dateBlock();
@@ -107,9 +108,10 @@ const taskWrapper2 = createDomElement('div', {
    className: 'todo__task-wrapper'
 });
 
-const btnDone2 = createDomElement('button', {
+const btnDone2 = createDomElement('input', {
    className: 'todo__item-done',
-   textContent: '\u2713'
+   //value: '\u2713',
+   type: 'checkbox',
 });
 
 const itemText2 = createDomElement('p', {
@@ -119,7 +121,7 @@ const itemText2 = createDomElement('p', {
 
 const btnItemDelete2 = createDomElement('button', {
    className: 'todo__item-delete',
-   textContent: '\u0078'
+   textContent: '\u0078',
 });
 
 const date2 = dateBlock();
@@ -143,10 +145,101 @@ taskWrapper2.append(btnItemDelete2);
 item2.append(taskWrapper2);
 item2.append(date2);
 
+items.append(item);
+items.append(item2);
+
 container.append(options);
-container.append(item);
-container.append(item2);
+container.append(items);
 
 todo.append(container);
 
 root.append(todo);
+
+btnAdd.addEventListener('click', () => {
+
+   const itemText = taskContent.value;
+
+   if (itemText.trim() === '') {
+      return;
+   }
+
+   const newItem = createDomElement('div', {
+      className: 'todo__item',
+   });
+
+   const newTaskWrapper = createDomElement('div', {
+      className: 'todo__task-wrapper'
+   });
+
+   const newBtnDone = createDomElement('input', {
+      className: 'todo__item-done',
+      //value: '\u2713',
+      type: 'checkbox',
+   });
+
+   const newItemText = createDomElement('p', {
+      className: 'todo__item-text',
+      textContent: itemText
+   });
+
+   const newBtnItemDelete = createDomElement('button', {
+      className: 'todo__item-delete',
+      textContent: '\u0078',
+   });
+
+   const newDate = dateBlock();
+
+   newTaskWrapper.append(newBtnDone);
+   newTaskWrapper.append(newItemText);
+   newTaskWrapper.append(newBtnItemDelete);
+
+   newItem.append(newTaskWrapper);
+   newItem.append(newDate);
+
+   items.append(newItem);
+
+   taskContent.value = ' ';
+});
+
+
+btnDelete.addEventListener('click', () => {
+
+   const isConfirmed = confirm('Вы действительно хотите удалить все Tasks?')
+   if (isConfirmed === false) {
+      return;
+   }
+
+   const allItemsToDel = document.querySelectorAll('.todo__item');
+
+   allItemsToDel.forEach((item) => {
+      item.remove();
+   });
+})
+
+
+items.addEventListener('click', (e) => {
+
+   if (e.target.classList.contains('todo__item-delete')) {
+      const targetItemToDel = e.target.closest('.todo__item');
+      targetItemToDel.remove();
+   };
+})
+
+items.addEventListener('click', (e) => {
+
+   if (e.target.classList.contains('todo__item-done')) {
+      const targetItemToChange = e.target.closest('.todo__item');
+      const targetTextToChange = targetItemToChange.querySelector('.todo__item-text');
+
+
+
+      if (e.target.checked) {
+         targetItemToChange.style.backgroundColor = 'rgb(173, 208, 179)';
+         targetTextToChange.style.textDecoration = 'line-through';
+
+      } else {
+         targetItemToChange.style.backgroundColor = '';
+         targetTextToChange.style.textDecoration = '';
+      }
+   }
+})

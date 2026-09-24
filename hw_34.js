d0df8d36ee5e3fa115a1e7d2eb066646710fdf61
Volokhh <karinochkavolokh@gmail.com>
2026-09-24 14:59:55 +0300

    HW-34_retrospective_minor-fixed

--- hw_34.js
+++ hw_34.js
@@ -30,37 +30,20 @@ const subjects = {
 console.log(Object.keys(subjects).join(', '));
 
 //2
-//Вариант-1
 const subjectsArr = Object.entries(subjects);
 
-const totalAmountOfStudentsAndTeachers = subjectsArr.reduce((acc, [key, value]) => {
-   return acc + value.students + value.teachers;
-}, 0);
-console.log(`Общее количество студентов и учителей на всех предметах: ${totalAmountOfStudentsAndTeachers}`);
-
-//Вариант-2
-let totalAmount = 0;
-for (const key in subjects) {
-
-   const amountOfStudentsAndTeachers = subjects[key]
-
-   for (const key in amountOfStudentsAndTeachers) {
-      totalAmount += amountOfStudentsAndTeachers[key]
-   }
-}
-console.log(`Общее количество студентов и учителей на всех предметах: ${totalAmount}`)
-
-//3
-//Вариант-1
 const totalAmountOfStudents = subjectsArr.reduce((acc, [key, value]) => {
    return acc + value.students;
 }, 0);
+console.log(`Общее количество студентов на всех предметах: ${totalAmountOfStudents}`);
 
+const totalAmountOfTeachers = subjectsArr.reduce((acc, [key, value]) => {
+   return acc + value.teachers;
+}, 0);
+console.log(`Общее количество учителей на всех предметах: ${totalAmountOfTeachers}`);
+
+//3
 console.log(`Среднее количество студентов на всех предметах: ${totalAmountOfStudents / subjectsArr.length}`);
-
-//Вариант-2
-console.log(`Среднее количество студентов на всех предметах: ${(subjects.mathematics.students + subjects.biology.students + subjects.chemistry.students + subjects.geography.students) / Object.keys(subjects).length}`)
-
 
 //4
 //либо так, как было const subjectsArr = Object.entries(subjects);, либо так
